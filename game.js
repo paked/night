e5f716e207410e612053c059c980427edd4589aa
Harrison <harrison@theshoebridges.com>
2015-08-16 21:08:20 +1000

    Add platforms

--- game.js
+++ game.js
@@ -3,9 +3,11 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 function preload() {
     console.log("preloading...");
     game.load.image('player', 'assets/player.png');
+    game.load.image('platform', 'assets/platform.png');
 }
 
 var player;
+var platforms;
 var cursors;
 var jumpButton;
 var jumpTime = 0;
@@ -21,12 +23,18 @@ function create() {
     player.body.bounce.y = 0.05;
     player.body.collideWorldBounds = true;
 
+    platforms = this.add.physicsGroup();
+    platforms.create(200, 200, 'platform');
+
+    platforms.setAll('body.allowGravity', false);
+    platforms.setAll('body.immovable', true);
 
     cursors = game.input.keyboard.createCursorKeys();
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update() {
+    game.physics.arcade.collide(player, platforms);
     game.backgroundColor = '#ff0000';
 
     player.body.velocity.x = 0;
@@ -39,7 +47,7 @@ function update() {
         player.body.velocity.x = 200;
     }
 
-    if (cursors.up.isDown && player.body.onFloor() && game.time.now > jumpTime) {
+    if (cursors.up.isDown && (player.body.touching.down || player.body.onFloor()) && game.time.now > jumpTime) {
         player.body.velocity.y = -150;
         jumpTime = game.time.now + 750;
     }
